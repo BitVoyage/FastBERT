76033df88f3aa08f61d3a1821548b3d3eda81e40
lshzhang <zhanglusheng@outlook.com>
2020-06-03 11:22:46 +0800

    fix load max N bug

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@
 import collections
 import json
 import os
+import re
 import torch
 import torch.utils.data as data
 from model_define.optimization import BERTAdam
@@ -23,12 +24,12 @@ def eval_pr(labels, preds):
             FP += 1
         elif label == 0 and pred == 1:
             FN += 1
-    print('TP', TP)
-    print('TN', TN)
-    print('FP', FP)
-    print('FN', FN)
-    precise = TP/(TP+FN)
-    recall = TP/(TP+FP)
+    #print('TP', TP)
+    #print('TN', TN)
+    #print('FP', FP)
+    #print('FN', FN)
+    precise = TP/(TP+FN+0.0001)
+    recall = TP/(TP+FP+0.0001)
     return precise, recall
 
 
@@ -74,7 +75,14 @@ def save_model(path, model, epoch):
 def load_saved_model(model, saved_model_path, model_file=None):
     if model_file == None:
         files = os.listdir(saved_model_path)
-        model_file = sorted(files)[-1]
+        max_idx = 0
+        max_fname = ''
+        for fname in files:
+            idx = re.sub('Epoch_|\.bin', '',fname)
+            if int(idx) > max_idx:
+                max_idx = int(idx)
+                max_fname = fname
+        model_file = max_fname
     model_file = os.path.join(saved_model_path, model_file)
     model_weight = torch.load(model_file, map_location="cpu")
     new_state_dict = collections.OrderedDict()
@@ -86,6 +94,7 @@ def load_saved_model(model, saved_model_path, model_file=None):
         new_state_dict[name] = v
     model.load_state_dict(new_state_dict)
     logging.info('loaded saved model file:%s', model_file)
+    return model_file
 
 
 def init_bert_adam_optimizer(model, training_data_len, epoch, batch_size,
